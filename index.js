5cc565d12af36b496685dcd2efb8996de8dd5d72
csmahmud99 <csmahmud.2021@gmail.com>
2023-06-12 23:26:36 +0600

    JWT installed and access token secret created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// JWT backend-integration
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
